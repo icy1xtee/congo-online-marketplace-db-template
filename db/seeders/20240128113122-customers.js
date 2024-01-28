2322c05b5215d9321e4422ea037969690bd7e797
icy1xtee <easton2904@mail.ru>
2024-01-28 14:45:15 +0300

    json templates created

--- db/seeders/20240128113122-customers.js
+++ db/seeders/20240128113122-customers.js
@@ -1,25 +1,13 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Customers', [{
+      name: 'John Doe',
+      isBetaMember: false,
+    }], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Customers', null, {});
+  },
 };
